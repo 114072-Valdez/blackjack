ab4f54f24e70d6ca7036f3173b0782259f608334
Valdez Gerardo TUP <128557123+114072-Valdez@users.noreply.github.com>
2024-06-11 18:58:20 -0300

    Initial commit

---
+++ src/main/java/ar/edu/utn/frc/tup/lc/iii/entities/DummyEntity.java
@@ -0,0 +1,22 @@
+package ar.edu.utn.frc.tup.lc.iii.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Dummies")
+public class DummyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "description")
+    private String dummy;
+}
